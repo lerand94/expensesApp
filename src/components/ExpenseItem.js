95a8e6719cc8234893c63943664b1663f4d02dc6
Kirill <27140985+lerand94@users.noreply.github.com>
2022-07-16 02:17:25 +0300

    add some styles for ExpenseItem Component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,11 @@
+import "./ExpenseItem.css";
 const ExpenseItem = () => {
   return (
-    <div>
+    <div className="expense-item">
       <div>March 28th 2012</div>
-      <div>
+      <div className="expense-item__description">
         <h2>Car insurance</h2>
-        <div>$297.68</div>
+        <div className="expense-item__price">$297.68</div>
       </div>
     </div>
   );
