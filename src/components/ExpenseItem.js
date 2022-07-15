3f6a9488276a6b605162a8d0a6bcfd21e09d363a
Kirill <27140985+lerand94@users.noreply.github.com>
2022-07-16 02:29:13 +0300

    remove hardcoded from html and replaced on variable for outputting dynamic data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,16 @@
 import "./ExpenseItem.css";
+
 const ExpenseItem = () => {
+  const expenseDate = new Date(2021, 2, 28);
+  const expenseTitle = "Car insurance";
+  const expenseAmount = 297.68;
+
   return (
     <div className="expense-item">
-      <div>March 28th 2012</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car insurance</h2>
-        <div className="expense-item__price">$297.68</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
