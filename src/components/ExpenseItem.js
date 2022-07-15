a0cc15465f014970e4926f994a9789ab0d3ee506
Kirill <27140985+lerand94@users.noreply.github.com>
2022-07-15 19:49:02 +0300

    add one root element to return in ExpenseItem Component and add more Dummy data for example

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,13 @@
 const ExpenseItem = () => {
-  return <h2>Expense Item!</h2>;
+  return (
+    <div>
+      <div>March 28th 2012</div>
+      <div>
+        <h2>Car insurance</h2>
+        <div>$297.68</div>
+      </div>
+    </div>
+  );
 };
 
 export default ExpenseItem;
