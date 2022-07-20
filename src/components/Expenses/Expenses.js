4e4b042852e93fb9a6ae7b6412d0504b000dcba2
Kirill <27140985+lerand94@users.noreply.github.com>
2022-07-20 22:56:09 +0300

    filtered Expenses to render correctly

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -10,14 +10,19 @@ const Expenses = (props) => {
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
+  const filteredExpenses = props.items.filter((item) => {
+    return filteredYear === item.date.getFullYear().toString();
+  });
+
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {props.items.map((expense) => (
+      {filteredExpenses.map((expense) => (
         <ExpenseItem
+          key={expense.id}
           title={expense.title}
           amount={expense.amount}
           date={expense.date}
