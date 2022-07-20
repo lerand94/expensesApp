717db5e990ea4bce2e406fe4637ab9b23359b5e7
Kirill <27140985+lerand94@users.noreply.github.com>
2022-07-20 19:18:17 +0300

    Revert "realise very imporatant pattern Child-to-Parent communication. Passing data from child to parent. Child execute function from parents props, parent got this data, copy and added a new property like ID, finally consoled this"
    This reverts commit ef69d6e7e58687d1218695482728f13d2f88cce3.

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -3,16 +3,9 @@ import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = () => {
-  const saveExpenseDataHandler = (enteredData) => {
-    const expenseData = {
-      ...enteredData,
-      id: Math.random().toString(),
-    };
-    console.log(expenseData);
-  };
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      <ExpenseForm />
     </div>
   );
 };
