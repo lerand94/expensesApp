6c7321cb6bb4682e3cd40faf85b5b91268aab36f
Kirill <27140985+lerand94@users.noreply.github.com>
2022-07-20 18:40:13 +0300

    added submit handler for listening a submit event of form. And consoled outputting data

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,38 +2,50 @@ import "./ExpenseForm.css";
 import { useState } from "react";
 
 const ExpenseForm = () => {
-  // const [enteredTitle, setEnteredTitle] = useState("");
-  // const [enteredAmount, setEnteredAmount] = useState("");
-  // const [enteredDate, setEnteredDate] = useState("");
-  const [userInput, setUserInput] = useState({
-    enteredTitle: "",
-    enteredAmount: "",
-    enteredDate: "",
-  });
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitle: "",
+  //   enteredAmount: "",
+  //   enteredDate: "",
+  // });
 
   const titleChangeHandler = (e) => {
-    // setEnteredTitle(e.target.value);
+    setEnteredTitle(e.target.value);
     // setUserInput({ ...userInput, enteredTitle: e.target.value });
-    setUserInput((prevState) => {
-      return { ...prevState, enteredTitle: e.target.value };
-    });
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredTitle: e.target.value };
+    // });
   };
   const amountChangeHandler = (e) => {
-    // setEnteredAmount(e.target.value);
+    setEnteredAmount(e.target.value);
     // setUserInput({ ...userInput, enteredAmount: e.target.value });
-    setUserInput((prevState) => {
-      return { ...prevState, enteredAmount: e.target.value };
-    });
+    //   setUserInput((prevState) => {
+    //     return { ...prevState, enteredAmount: e.target.value };
+    //   });
   };
   const dateChangeHandler = (e) => {
-    // setEnteredDate(e.target.value);
+    setEnteredDate(e.target.value);
     // setUserInput({ ...userInput, enteredDate: e.target.value });
-    setUserInput((prevState) => {
-      return { ...prevState, enteredDate: e.target.value };
-    });
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredDate: e.target.value };
+    // });
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const enteredData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+
+    console.log(enteredData);
   };
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
